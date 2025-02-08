d510c1b143e5c9a80280d8363441153664694a87
Torycd <vicidundun@gmail.com>
2025-02-08 11:23:27 +0100

    commit

--- Project_One/src/Component/Navigation.jsx
+++ Project_One/src/Component/Navigation.jsx
@@ -1,4 +1,5 @@
 // import { useState } from "react";
+import { } from 'react-icons/fa'
 
 const Navigation = () => {
 //   const [showNav, setShowNav] = useState();
@@ -6,8 +7,15 @@ const Navigation = () => {
 
   return (
     <div className="flex justify-between lg:mx-20 lg:my-5">
-      <h2 className="text-[26px] font-bold " >INSURE</h2>
-      <div></div>
+      <h2 className="text-[26px] font-bold font-Karla" >INSURE</h2>
+      <div className="hidden lg:block">
+        <ul className="flex uppercase space-x-5">
+            <li>How we work</li>
+            <li>Blog</li>
+            <li>Account</li>
+        </ul>
+      </div>
+      <div className="md:hidden" >h</div>
     </div>
   );
 };
