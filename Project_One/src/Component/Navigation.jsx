2a57657954fa207a1ab9ee853af975956cca52b2
Torycd <vicidundun@gmail.com>
2025-02-08 10:26:06 +0100

    commit

--- Project_One/src/Component/Navigation.jsx
+++ Project_One/src/Component/Navigation.jsx
@@ -1,10 +1,15 @@
-import { useState } from "react";
+// import { useState } from "react";
 
 const Navigation = () => {
-  const [] = useState();
-  const [] = useState();
+//   const [showNav, setShowNav] = useState();
+//   const [navigation, setNavigation] = useState();
 
-  return <div>Navigation</div>;
+  return (
+    <div className="flex justify-between lg:mx-20 lg:my-5">
+      <h2 className="text-[26px] font-bold " >INSURE</h2>
+      <div></div>
+    </div>
+  );
 };
 
 export default Navigation;
