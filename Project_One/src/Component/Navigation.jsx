59c7335b1058912a0cd3720eee6473c72d932abe
Torycd <vicidundun@gmail.com>
2025-02-08 19:48:50 +0100

    commit

--- Project_One/src/Component/Navigation.jsx
+++ Project_One/src/Component/Navigation.jsx
@@ -10,7 +10,7 @@ const Navigation = () => {
 
   return (
     <div className="flex justify-between max-w-[1440px] m-5 md:mx-auto md:my-5 items-center">
-      <h2 className="text-[26px] font-bold font-Serif">INSURE</h2>
+      <h2 className="text-[26px] font-medium tracking-widest font-Serif">INSURE</h2>
       <div className="hidden lg:block">
         <ul className="flex uppercase space-x-10 text-[16px] font-semibold items-center">
           <li className={style}>How we work</li>
