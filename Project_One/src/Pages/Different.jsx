8a58d1dd8890f202b9cf1e5c70768e2d173e52b8
Torycd <vicidundun@gmail.com>
2025-02-11 09:24:01 +0100

    commit

--- Project_One/src/Pages/Different.jsx
+++ Project_One/src/Pages/Different.jsx
@@ -2,8 +2,11 @@
 
 const Different = () => {
   return (
-    <div>Dif</div>
-  )
-}
+    <div className="mt-[200px] max-w-[1440px] mx-auto">
+      <hr className="w-[200px] text-black text-[24px] z-100" />
+      <h2 className="text-[60px]">We&apos;re are different</h2>
+    </div>
+  );
+};
 
-export default Different
+export default Different;
