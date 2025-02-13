fa77debb502c446013399cc1e5882b33bc569c0a
Torycd <vicidundun@gmail.com>
2025-02-13 06:58:06 +0100

    commit

--- Project_One/src/Component/Navigation.jsx
+++ Project_One/src/Component/Navigation.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { FaBars, FaCross } from "react-icons/fa";
 
 import Button from "./Button";
-import Logo from "../assets/images/logo.svg"
+import Logo from "../assets/images/logo.svg";
 
 const Navigation = () => {
   const [navigation, setNavigation] = useState(false);
@@ -30,7 +30,7 @@ const Navigation = () => {
           className="md:hidden p-2 border-2 border-black"
           onClick={handleNavigation}
         >
-          {navigation ? <FaCross size={20} /> :  <FaBars size={20} /> }
+          {navigation ? <FaCross size={20} /> : <FaBars size={20} />}
         </div>
       </div>
       <div
