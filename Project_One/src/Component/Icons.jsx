aebcb76d8ac487a8da7c6771f7930bb57b8b25d5
Torycd <vicidundun@gmail.com>
2025-02-15 08:29:44 +0100

    commit

--- Project_One/src/Component/Icons.jsx
+++ Project_One/src/Component/Icons.jsx
@@ -1,10 +1,21 @@
 // import React from 'react'
 import Logo from "../assets/images/logo.svg"
+import Facebook from "../assets/images/logo.svg"
+import Twitter from "../assets/images/logo.svg"
+import Instagram from "../assets/images/logo.svg"
+import Pinterest from "../assets/images/logo.svg"
 
 const Icons = () => {
   return (
     <div className="flex justify-between">
         <img src={Logo} alt="logo"  className="" />
+        <div className="flex justify-between">
+            <img src={Facebook} alt="icons"  />
+            <img src={Twitter} alt="icons"  />
+            <img src={Instagram} alt="icons"  />
+            <img src={Pinterest} alt="icons"  />
+            
+        </div>
     </div>
   )
 }
--- Project_One/src/Pages/Footer.jsx
+++ Project_One/src/Pages/Footer.jsx
@@ -1,13 +1,15 @@
 // import React from 'react'
 
+import Icons from "../Component/Icons";
+
 const Footer = () => {
   return (
     <div className="bg-gray-50">
-        <div className="max-w-[1440px] mx-[20px] sm:mx-auto" >
-            
-        </div>
+      <div className="max-w-[1440px] mx-[20px] sm:mx-auto">
+        <Icons />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
