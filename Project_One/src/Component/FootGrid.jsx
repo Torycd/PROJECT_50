7391b6b05aba42a1c3e4193af7138167776e2489
Torycd <vicidundun@gmail.com>
2025-02-15 16:27:34 +0100

    commit

--- Project_One/src/Component/FootGrid.jsx
+++ Project_One/src/Component/FootGrid.jsx
@@ -6,33 +6,81 @@ const FootGrid = () => {
       <div>
         <h2 className="mb-[30px] opacity-50 font-bold">Our company</h2>
         <ul className="flex flex-col gap-2 font-semibold">
-          <li><a href="">How we work</a></li>
-          <li><a href="">Why Insure?</a></li>
-          <li><a href="">View plans</a></li>
+          <li>
+            <a href="" className="hover:underline">
+              How we work
+            </a>
+          </li>
+          <li>
+            <a href="" className="hover:underline">
+              Why Insure?
+            </a>
+          </li>
+          <li>
+            <a href="" className="hover:underline">
+              View plans
+            </a>
+          </li>
         </ul>
       </div>
       <div>
         <h2 className="mb-[30px] opacity-50 font-bold">FAQ</h2>
         <ul className="flex flex-col gap-2 font-semibold">
-          <li><a href="">Terms of use</a></li>
-          <li><a href="">Privacy policy</a></li>
-          <li><a href="">Cookies</a></li>
+          <li>
+            <a href="" className="hover:underline">
+              Terms of use
+            </a>
+          </li>
+          <li>
+            <a href="" className="hover:underline">
+              Privacy policy
+            </a>
+          </li>
+          <li>
+            <a href="" className="hover:underline">
+              Cookies
+            </a>
+          </li>
         </ul>
       </div>
       <div>
         <h2 className="mb-[30px] opacity-50 font-bold">Contact</h2>
         <ul className="flex flex-col gap-2 font-semibold">
-          <li><a href="">Sales</a></li>
-          <li><a href="">Support</a></li>
-          <li><a href="">Live chat</a></li>
+          <li>
+            <a href="" className="hover:underline">
+              Sales
+            </a>
+          </li>
+          <li>
+            <a href="" className="hover:underline">
+              Support
+            </a>
+          </li>
+          <li>
+            <a href="" className="hover:underline">
+              Live chat
+            </a>
+          </li>
         </ul>
       </div>
       <div>
         <h2 className="mb-[30px] opacity-50 font-bold">Others</h2>
         <ul className="flex flex-col gap-2 font-semibold">
-          <li><a href="">Careers</a></li>
-          <li><a href="">Press</a></li>
-          <li><a href="">Licenses</a></li>
+          <li>
+            <a href="" className="hover:underline">
+              Careers
+            </a>
+          </li>
+          <li>
+            <a href="" className="hover:underline">
+              Press
+            </a>
+          </li>
+          <li>
+            <a href="" className="hover:underline">
+              Licenses
+            </a>
+          </li>
         </ul>
       </div>
     </div>
