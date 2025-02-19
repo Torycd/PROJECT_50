34d2e949e6efff3b981c752f6a24b8031e4d0dbb
Torycd <vicidundun@gmail.com>
2025-02-19 09:28:15 +0100

    commit

--- Project_TWO/src/App.jsx
+++ Project_TWO/src/App.jsx
@@ -1,9 +1,14 @@
 // import React from 'react'
 
+import Items from "./Components/Items";
+
 const App = () => {
   return (
-    <div className="flex justify-between">
-      <div>Items</div>
+    <div className="flex justify-between m-20">
+      <div>
+        <h2 className="text-[32px] font-bold mb-[40px]">Desserts</h2>
+        <Items />
+      </div>
       <div>Cart</div>
     </div>
   );
--- Project_TWO/src/Components/Items.jsx
+++ Project_TWO/src/Components/Items.jsx
@@ -1,9 +1,95 @@
 // import React from 'react'
+import image from "../assets/images/image-cake-desktop.jpg";
 
 const Items = () => {
+  const DUMMY_ITEMS = [
+    {
+      type: "Waffle",
+      name: "Waffle with Berries",
+      price: 6.5,
+      image: "",
+    },
+    {
+      type: "Crème Brûlée",
+      name: "Vanilla Bean Crème Brûlée",
+      price: 7.0,
+      image: "",
+    },
+    {
+      type: "Macaron",
+      name: "Macaron Mix of Five",
+      price: 8.0,
+      image: "",
+    },
+    {
+      type: "Tiramisu",
+      name: "Classic Tiramisu",
+      price: 5.5,
+      image: "",
+    },
+    {
+      type: "Tiramisu",
+      name: "Classic Tiramisu",
+      price: 5.5,
+      image: "",
+    },
+    {
+      type: "Baklava",
+      name: "Pistachio Baklava",
+      price: 4.0,
+      image: "",
+    },
+    {
+      type: "Pie",
+      name: "Lemon Meringue Pie",
+      price: 5.0,
+      image: "",
+    },
+    {
+      type: "Cake",
+      name: "Red Velvet Cake",
+      price: 4.5,
+      image: "",
+    },
+    {
+      type: "Brownie",
+      name: "Salted Caramel Brownie",
+      price: 4.5,
+      image: "",
+    },
+    {
+      type: "Panna Cotta",
+      name: "Vanilla Panna Cotta",
+      price: 6.5,
+      image: "",
+    },
+  ];
   return (
-    <div>Items</div>
-  )
-}
+    <div className="grid grid-cols-4">
+      {/* card */}
+      <div className="overflow-hidden hover:shadow-lg transition-shadow duration-300"
+      >
+        <div className="relative h-48">
+          <img
+            src={image}
+            alt=""
+            className="w-full h-full rounded-md object-cover"
+          />
+          <div className="absolute top-2 right-2 bg-white px-2 py-1 rounded-full">
+            <span className="text-sm font-semibold">
+              5.00
+            </span>
+          </div>
+        </div>
+        <div className="p-4">
+          <div className="mb-1">
+            <span className="text-sm text-gray-500">htht</span>
+          </div>
+          <h3 className="text-lg font-semibold line-clamp-2">hh</h3>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Items
+export default Items;
